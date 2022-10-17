a57883726713855a1de723cbac8e180216826a00
Georgii Kuzmin <georgii@scal.io>
2022-10-17 15:27:10 +0400

    feat(app): not found redirect added

--- src/app/modules/books/services/book-resolver.service.ts
+++ src/app/modules/books/services/book-resolver.service.ts
@@ -1,17 +1,29 @@
 import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, Resolve} from "@angular/router";
+import {ActivatedRouteSnapshot, Resolve, Router} from "@angular/router";
 import {Book} from "../models/book.interface";
 import books from '../mock/books';
 import {delay, map, of, tap} from "rxjs";
 
 @Injectable()
 export class BookResolverService implements Resolve<Book | null> {
+  constructor(private readonly router: Router) {}
+
   public resolve(route: ActivatedRouteSnapshot) {
     const id = route.paramMap.get('id') || 0;
 
     return of<Book[]>(books).pipe(
       delay(300),
-      map(books => books.find(book => book.id === +id) || null),
+      map(books => {
+        const currBook = books.find(book => book.id === +id);
+        if (!currBook) {
+          alert(`There is no book with ${id} id`);
+          void this.router.navigate(['all']);
+
+          return null;
+        }
+
+        return currBook;
+      }),
       tap(() => console.log('Data resolved!')),
     );
   }
